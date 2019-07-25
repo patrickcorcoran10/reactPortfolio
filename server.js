d5f0dd94c10d7dbcc17fa920e0bfc49908b3c66c
Patty C <patrickcorcoran10@gmail.com>
2019-07-25 14:53:50 -0500

    commit that fixed the email issue

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
-const routes = require("./routes");
+const nodemailer = require('nodemailer');
 const app = express();
+
+require("dotenv").config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -14,13 +17,50 @@ if (process.env.NODE_ENV === "production") {
   }
   
   // Define API routes here
-  app.use(routes);
+  // app.use(routes);
   
   // Send every other request (anything else) to the React app
   // Define any API routes before this runs
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
+
+  app.post('/submit', (req, res) => {
+    console.log(req.body)
+    nodemailer.createTestAccount((err, account) => {
+        const htmlEmail = `
+            <h3>Contact Details</h3>
+            <ul>
+                <li>Name: ${req.body.name}</li>
+                <li>Email: ${req.body.email}</li>
+            </ul>
+            <h3>Message</h3>
+            <p>${req.body.message}</p>
+        `
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "p.corcoran.portfolio@gmail.com",
+                pass: process.env.PASS
+            }
+        })
+
+        let info = transporter.sendMail({
+            from: "website@p-corcoran-portfolio.com",
+            to: "patrickcorcoran10@gmail.com",
+            subject: "P Corcoran Portfolio Inquiry",
+            text: "req.body.message",
+            html: htmlEmail
+        })
+
+        console.log("Message sent: %s", info.text);
+
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info) )
+        })
+    })
+
   
   // Syncing our database and logging a message to the user upon success
   // using db.sequelize.sync({force: true}).then(function() { will reset db every time.
